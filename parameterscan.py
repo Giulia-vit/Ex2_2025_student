9e8ed8729778a964988274bb94a24371d7ebbd13
nberger-new <n.bergerhrynova@gmail.com>
2025-03-11 15:02:13 +0100

    Most of question 1 and 2

--- parameterscan.py
+++ parameterscan.py
@@ -3,15 +3,17 @@ import subprocess
 import matplotlib.pyplot as plt
 import os
 
+# Variation de l'énergie : variation par rapport à la position précédente : (E[i] - E[i+1])/dt
+
 # Parameters
 executable = './Ex2_2025_student'  # Nome dell'eseguibile
-repertoire = r"/home/nhb/python/python-schemdraw/Physique-Numerique/Exercice2"
+repertoire = r"/home/nhb/python/python-schemdraw/Physique-Numerique/Ex2_2025_student"
 os.chdir(repertoire)
 
 input_filename = 'configuration.in.example'  # Nome del file di input
 
 #number of steps and simulations
-nsteps = np.array([1, 5, 10, 20, 50, 100, 200, 500, 1000]) # TODO change
+nsteps = np.array([5, 20, 200, 1000]) # TODO change
 nsimul = len(nsteps)  # Number of simulations to perform
 
 tfin = 62.8319
@@ -40,6 +42,7 @@ om_0 = 7.07
 lw = 1
 fs = 16
 
+j = 0
 for nstep in nsteps :
 # opening our file
     try:
@@ -51,38 +54,51 @@ for nstep in nsteps :
         Pnc = data[:, 4] # Puissance des forces non-conservatives
         convergence_list_pos.append(theta[-1])
         convergence_list_speed.append(theta_dot[-1])
+
     except Exception as e:
         print(f"Errore nel caricamento dei dati: {e}")
         exit()
-    # Plot dei risultati
-    # color1 = np.random.rand(3,)
-    # color2 = np.random.rand(3,)
-    # fig, ax = plt.subplots(constrained_layout=True)
-    # ax.plot(t, theta, marker='.', label='Position', color=color1, linewidth=lw)
-    # ax.plot(t, theta_dot, marker='.', label='Speed', color=color2, linewidth=lw)
-    # ax.set_xlabel('Tempo [s]', fontsize=fs)
-    # ax.set_ylabel('Valore [unità]', fontsize=fs)
-    # ax.set_title(f'nstep={nstep}')
-    # ax.legend(fontsize=fs)
-    # ax.grid(True)
+
+    color1 = np.random.rand(3,)
+    color2 = np.random.rand(3,)
+    fig, ax = plt.subplots(constrained_layout=True)
+    ax.plot(t, theta, label='Position', color=color1, linewidth=lw)
+    ax.plot(t, theta_dot, label='Speed', color=color2, linewidth=lw)
+    ax.set_xlabel('Tempo [s]', fontsize=fs)
+    ax.set_ylabel('Valore [unità]', fontsize=fs)
+    ax.set_title(f'nstep={nstep}')
+    ax.legend(fontsize=fs)
+    ax.grid(True)
 
     fig, ax = plt.subplots(constrained_layout=True)
     fig, ax1 = plt.subplots(constrained_layout=True)
 
-    # Plot dei risultati
-    color1 = np.random.rand(3,)
-    ax.plot(theta, theta_dot, marker='.', label=f'nstep={nstep}', color=color1, linewidth=lw)
+    ax.plot(theta, theta_dot, label=f'nstep={nstep}', color=color1, linewidth=lw)
     ax.set_xlabel('Position [m]', fontsize=fs)
     ax.set_ylabel('Speed [m/s]', fontsize=fs)
     ax.set_title(r'Vitesse en fonction de la position')
     ax.legend(fontsize=fs)
     ax.grid(True)
 
-    ax1.plot(E, t, marker='.', label=f'nstep={nstep}', color=color1, linewidth=lw)
+    ax1.plot(E, t, label=f'nstep={nstep}', color=color1, linewidth=lw)
     ax1.set_xlabel('Energie', fontsize=fs)
     ax1.set_ylabel('Temps [s]', fontsize=fs)
     ax1.legend(fontsize=fs)
     ax1.grid(True)
+
+    d_E = []
+    for i in range (len(E) - 1) :
+        d_E.append((E[i+1] - E[i])/dt[j])
+    d_E.append(0)
+
+    fig, ax3 = plt.subplots(constrained_layout=True)
+    ax3.plot(t, d_E, label='Derivative energy', color=color1, linewidth=lw)
+    ax3.set_xlabel('Temps [s]', fontsize=fs)
+    ax3.set_ylabel("Dérivée de l'énergie [unità]", fontsize=fs)
+    ax3.set_title(f'nstep={nstep}')
+    ax3.legend(fontsize=fs)
+    ax3.grid(True)
+    j = j+1
 
 d_x = [om_0**2*(x - th_pos)**2 for x in convergence_list_pos]
 d_v = [(x - th_speed)**2 for x in convergence_list_speed]
